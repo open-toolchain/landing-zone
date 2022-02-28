f5a9b7119331a2d058ed09df2a6bd18031f222be
Kimberly Hunter <Kimberly.Hunter@ibm.com>
2022-02-28 11:29:09 -0500

    Create atracker.tf

---
+++ atracker.tf
@@ -0,0 +1,26 @@
+##############################################################################
+# Atracker
+##############################################################################
+
+data "ibm_resource_group" "cos_group" {
+  name = var.resource_group
+}
+
+resource "ibm_atracker_target" "atracker_target" {
+  cos_endpoint {
+    endpoint   = "s3.private.${var.location}.cloud-object-storage.appdomain.cloud"
+    target_crn = var.target_crn
+    bucket     = var.prefix != null ? "${var.prefix}-${var.bucket_name}" : var.bucket_name
+    api_key    = var.api_key
+  }
+  name        = (var.atracker_target_name != null ? var.atracker_target_name : "atracker-target-${var.location}")
+  target_type = var.atracker_target_type
+}
+
+resource "ibm_atracker_route" "atracker_route" {
+  name                  = (var.atracker_route_name != null ? var.atracker_route_name : "atracker-route-${var.location}")
+  receive_global_events = var.receive_global_events
+  rules {
+    target_ids = [ibm_atracker_target.atracker_target.id]
+  }
+}
