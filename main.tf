8a14da4e2dd5164a4c53bfa7affddc72badd680c
Kimberly Hunter <Kimberly.Hunter@ibm.com>
2022-02-28 11:12:05 -0500

    Removal of cos, cos bucket and data block

--- main.tf
+++ main.tf
@@ -130,40 +130,6 @@ resource "ibm_is_flow_log" "flow_logs" {
 ##############################################################################
 # Activity Tracker with COS 
 ##############################################################################
-data "ibm_resource_group" "cos_group" {
-  name = var.resource_group
-}
-
-module "cos" {
-  source  = "terraform-ibm-modules/cos/ibm//modules/instance"
-  version = "1.4.1"
-
-  service_name      = var.prefix != null ? "${var.prefix}-${var.service_name}" : var.service_name
-  plan              = var.plan
-  region            = var.region
-  resource_group_id = data.ibm_resource_group.cos_group.id
-  parameters        = var.parameters
-
-  bind_resource_key = var.bind_resource_key
-  resource_key_name = var.resource_key_name
-  key_parameters    = var.key_parameters
-  role              = var.role
-}
-
-module "cos_bucket" {
-  source  = "terraform-ibm-modules/cos/ibm//modules/bucket"
-  version = "1.4.1"
-
-  bucket_name     = var.prefix != null ? "${var.prefix}-${var.bucket_name}" : var.bucket_name
-  cos_instance_id = module.cos.cos_instance_id
-  location        = var.location
-  storage_class   = var.storage_class
-  force_delete    = var.force_delete
-  endpoint_type   = var.endpoint_type
-  kms_key_crn     = var.kms_key_crn
-  expire_rules    = var.expire_rules
-}
-
 module "atracker" {
  //Uncomment link below line to make it point to registry level
   source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-atracker.git"
