3bab70240e26256f8bf583bbeb09f8cf91ee071e
Kimberly Hunter <Kimberly.Hunter@ibm.com>
2022-03-11 15:50:50 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -84,8 +84,8 @@ resource "ibm_container_vpc_cluster" "cluster" {
   dynamic "kms_config" {
     for_each = each.value.kms_config == null ? [] : [each.value.kms_config] 
     content {
-      crk_id      = module.key_protect.key_map[kms_config.value.crk_name].id
-      instance_id = module.key_protect.kms_guid
+      crk_id      = module.key_management.key_map[kms_config.value.crk_name].id
+      instance_id = module.key_management.kms_guid
     }
   }
 
